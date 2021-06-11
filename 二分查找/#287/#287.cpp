ca0b91d6971d252b12cfd141eafd8a4cfa3b0ccd
Administrator <admin@example.com>
2021-06-11 23:33:23 +0800

    6-11

---
+++ 二分查找/#287/#287.cpp
@@ -0,0 +1,67 @@
+//法一：二分查找
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        int l=0,r=n-1,ans=-1;
+        while(l<=r){
+            int mid=(l+r)>>1;
+            int cnt = 0;
+            for (int i = 0; i < n; ++i) {
+                cnt += nums[i] <= mid;
+            }
+            if (cnt <= mid) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+                ans = mid;
+            }
+        }
+    }
+};
+
+//法二：二进制
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size(), ans = 0;
+        // 确定二进制下最高位是多少
+        int bit_max = 31;
+        while (!((n - 1) >> bit_max)) {
+            bit_max -= 1;
+        }
+        for (int bit = 0; bit <= bit_max; ++bit) {
+            int x = 0, y = 0;
+            for (int i = 0; i < n; ++i) {
+                if (nums[i] & (1 << bit)) {
+                    x += 1;
+                }
+                if (i >= 1 && (i & (1 << bit))) {
+                    y += 1;
+                }
+            }
+            if (x > y) {
+                ans |= 1 << bit;
+            }
+        }
+        return ans;
+    }
+};
+
+//法三：快慢指针
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = 0, fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+};
---
+++ 二叉搜索树/#538/#538.cpp
@@ -0,0 +1,64 @@
+法一：
+class Solution {
+    int sum=0;
+public:
+    TreeNode* convertBST(TreeNode* root) {   //右中左遍历树
+    if(root!=nullptr){
+        convertBST(root->right);
+        sum += root->val;
+        root->val = sum;
+        convertBST(root->left);
+    }
+    return root;
+    }
+};
+
+
+法二：
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* getSuccessor(TreeNode* node) {
+        TreeNode* succ = node->right;
+        while (succ->left != nullptr && succ->left != node) {
+            succ = succ->left;
+        }
+        return succ;
+    }
+
+    TreeNode* convertBST(TreeNode* root) {
+        int sum = 0;
+        TreeNode* node = root;
+
+        while (node != nullptr) {
+            if (node->right == nullptr) {
+                sum += node->val;
+                node->val = sum;
+                node = node->left;
+            } else {
+                TreeNode* succ = getSuccessor(node);
+                if (succ->left == nullptr) {
+                    succ->left = node;
+                    node = node->right;
+                } else {
+                    succ->left = nullptr;
+                    sum += node->val;
+                    node->val = sum;
+                    node = node->left;
+                }
+            }
+        }
+
+        return root;
+    }
+};
